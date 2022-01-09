02fa8cd358846d5aa11bca63b20738186c15b71d
Lohenl <winsobaggins@yahoo.com.sg>
2022-01-10 01:14:36 +0800

    docker compose integration

--- backend/src/data/clients/mongoose-client.js
+++ backend/src/data/clients/mongoose-client.js
@@ -15,5 +15,13 @@ if (!process.env.IS_LOCAL_TEST && process.env.IS_LOCAL_TEST !== 'true') {
   db.on('error', (err) => {
     logger.error('Connection error:', err);
   });
+} else if (!process.env.MONGO_PROD && process.env.IS_LOCAL_TEST !== 'true') {
+  const url = process.env.MONGODB_URL;
+
+  mongoose.connect(url, { 
+    useNewUrlParser: true,
+    username: process.env.MONGO_USER,
+    password: process.env.MONGO_PASSWORD,
+  });
 }
 module.exports = mongoose;
